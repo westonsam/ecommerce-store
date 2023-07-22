905a0e574041b19c544889d3f7c5a47f39a3b0dc
westonsam <132954237+westonsam@users.noreply.github.com>
2023-07-21 22:34:19 -0700

    Developed Billboard Component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,20 @@
-const HomePage = () => {
-    return ( 
-        <div>
-            Hello Store
-        </div>
-     );
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("b80c16ef-ca99-4d83-9644-eb60809eb49f");
+
+    return (
+        <Container>
+            <div
+                className="space-y-10 pb-10">
+                    <Billboard data={billboard}/>
+            </div>
+        </Container>
+    );
 }
- 
+
 export default HomePage;
---
+++ components/billboard.tsx
@@ -0,0 +1,25 @@
+import { Billboard } from "@/types";
+
+interface BillboardProps {
+    data: Billboard;
+}
+
+const Billboard: React.FC<BillboardProps> = ({
+    data
+}) => {
+    return (
+        <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+            <div 
+            className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+            style={{ backgroundImage: `url(${data?.imageUrl})`}}>
+                <div className="flex flex-xol justify-center items-center h-full w-full text-center gap-y-8">
+                    <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+                        {data.label}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Billboard;
